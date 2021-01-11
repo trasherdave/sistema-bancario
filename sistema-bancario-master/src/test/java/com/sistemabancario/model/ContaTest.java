a17ffb5dbb2db6c65ee1a890bf4e534053a12bd1
David Rodrigues <56417462+trasherdave@users.noreply.github.com>
2021-01-11 16:44:21 -0300

    Implementação de testes dos requisitos R01 a R04

---
+++ sistema-bancario-master/src/test/java/com/sistemabancario/model/ContaTest.java
@@ -0,0 +1,59 @@
+package com.sistemabancario.model;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ContaTest {
+
+    @Test
+    void testSetNumeroValido() {
+        final Conta instance = new Conta();
+        final String esperado = "12345-6";
+        instance.setNumero(esperado);
+        final String obtido = instance.getNumero();
+        assertEquals(esperado, obtido);
+    }
+
+    @Test
+    void testSetNumeroInvalidoNaoArmazena() {
+        final Conta instance = new Conta();
+        final String invalido = "123";
+        assertThrows(IllegalArgumentException.class, () -> instance.setNumero(invalido));
+        final String obtido = instance.getNumero();
+        assertNotEquals(invalido, obtido);
+    }
+
+    @Test
+    void testInstanciaPadraoPoupanca() {
+        final Conta instance = new Conta();
+        assertFalse(instance.isPoupanca());
+    }
+
+    @Test
+    void testSetLimiteContaEspecial() {
+        final Conta instance = new Conta();
+        instance.setEspecial(true);
+        final double esperado = 1000;
+        instance.setLimite(esperado);
+        final double obtido = instance.getLimite();
+        assertEquals(esperado, obtido);
+    }
+
+    @Test
+    void testSetLimiteContaNaoEspecial() {
+        final Conta instance = new Conta();
+        final double limite = 1000;
+        assertThrows(IllegalStateException.class, () -> instance.setLimite(limite));
+
+    }
+
+    @Test
+    void testHistoricoNotNull() {
+        final Conta instance = new Conta();
+        assertNotNull(instance.getMovimentacoes());
+
+    }
+
+   
+
+}
